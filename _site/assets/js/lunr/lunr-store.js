15d03eb9d9a7ef78dbaaef4b13c0f8077a29737f
junhobaik <junhobaik@gmail.com>
2018-01-15 05:14:35 +0900

    Remove _site directory

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run bundle exec jekyll serve, which launches a web server and auto-regenerates your...","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/welcome-to-jekyll/",
-        "teaser":null}]
