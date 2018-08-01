b79008656d561b45e542640c5ba54de2b3a34297
junhobaik <junhobaik@gmail.com>
2018-08-01 18:12:58 +0900

    blogTemplate에 기존 layout 적용

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Layout from '../components/layout'
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -7,6 +8,7 @@ export default function Template({
   const { markdownRemark } = data // data.markdownRemark holds our post data
   const { frontmatter, html } = markdownRemark
   return (
+    <Layout>
     <div className="blog-post-container">
       <div className="blog-post">
         <h1>{frontmatter.title}</h1>
@@ -17,6 +19,7 @@ export default function Template({
         />
       </div>
     </div>
+    </Layout>
   )
 }
 
