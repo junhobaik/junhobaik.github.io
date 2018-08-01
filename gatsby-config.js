7f6216e01211dc57735ae1d5515726dad0c3ece7
junhobaik <junhobaik@gmail.com>
2018-08-01 17:50:57 +0900

    블로그 포스트 구현, gatsby-source-filesystem 설치 및 적용

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/path/to/markdown/files`,
+        name: 'markdown-pages',
+      },
+    },
+  ],
 }
