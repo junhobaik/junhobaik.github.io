f0351a313fc1f68a5467f98115bff1da1aa8447f
junhobaik <junhobaik@gmail.com>
2018-08-03 22:19:01 +0900

    기존 stater demo의 문구 제거

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,12 +16,7 @@ const IndexPage = ({
 
   return (
     <Layout>
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-
       {Posts}
-
       <Link to="/page-2/">Go to page 2</Link>
     </Layout>
   )
