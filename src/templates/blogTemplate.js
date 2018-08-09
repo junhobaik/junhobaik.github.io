27e06ea551f79f435555186b206421bfbeef8ae8
junhobaik <junhobaik@gmail.com>
2018-08-09 20:52:26 +0900

    블로그 포스트 내 이전 다음 포스트 이동 기능 구현

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,40 +1,56 @@
-import React from "react"
-import { graphql } from "gatsby"
+import React from 'react'
+import { graphql, Link } from 'gatsby'
 import Layout from '../components/layout'
+import Helmet from 'react-helmet'
 
-export default function Template({
-  data, // this prop will be injected by the GraphQL query below.
-}) {
-  const { markdownRemark } = data // data.markdownRemark holds our post data
-  const { frontmatter, html } = markdownRemark
-  return (
-    <Layout>
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
-    </div>
-    </Layout>
-  )
+export default class Template extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const { data, location, pageContext } = this.props
+    const { markdownRemark } = data
+    const { frontmatter, html } = markdownRemark
+    const siteTitle = data.site.siteMetadata.siteTitle
+    const { previous, next } = pageContext
+
+    return (
+      <Layout location={location}>
+        <Helmet title={`${frontmatter.title} | ${siteTitle}`} />
+        <div className="blog-post-container">
+          <div className="blog-post">
+            <h1>{frontmatter.title}</h1>
+            <h2>{frontmatter.date}</h2>
+            <div
+              className="blog-post-content"
+              dangerouslySetInnerHTML={{ __html: html }}
+            />
+          </div>
+
+          <div className="blog-post-nav">
+            <ul>
+              {previous && (
+                <li>
+                  <Link to={previous.fields.slug} rel="prev">
+                    ← {previous.frontmatter.title}
+                  </Link>
+                </li>
+              )}
+
+              {next && (
+                <li>
+                  <Link to={next.fields.slug} rel="next">
+                    {next.frontmatter.title} →
+                  </Link>
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
 }
-
-// export const pageQuery = graphql`
-//   query($path: String!) {
-//     markdownRemark(frontmatter: { path: { eq: $path } }) {
-//       html
-//       frontmatter {
-//         date(formatString: "MMMM DD, YYYY")
-//         path
-//         title
-//       }
-//     }
-//   }
-// `
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
