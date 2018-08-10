a8f0d2c3b8f1c0d80e0e034407f02a5f3678bb68
junhobaik <junhobaik@gmail.com>
2018-08-10 18:15:08 +0900

    헤더 파일 제거

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
-import Header from './header'
 import './layout.scss'
 import profileImg from './profile.png';
 
