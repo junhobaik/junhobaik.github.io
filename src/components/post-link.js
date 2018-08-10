4e5e70aa9e0e5d83f197213a4d86d4baa2aba02a
junhobaik <junhobaik@gmail.com>
2018-08-10 18:37:26 +0900

    post-link 스타일 수정

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import get from 'lodash/get'
+import './post-link.scss'
 
 const PostLink = ({ post }) => {
-  const title = get(post, 'frontmatter.title') || post.fields.slug
+  const slug = post.fields.slug;
+  const title = get(post, 'frontmatter.title') || slug
+  const excerpt = post.excerpt;
+  // const date = post.frontmatter.date
 
   return (
     <div className="post-link" key={title}>
-      <Link to={post.fields.slug}>
-        {title} ({post.frontmatter.date})
+      <Link to={slug}>
+        <h2 className="title">{title}</h2>
+        <span className="excerpt">{excerpt}</span>
       </Link>
     </div>
   )
