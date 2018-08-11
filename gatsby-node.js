30db84de86b6561c100003b20535f69067eadda9
junhobaik <junhobaik@gmail.com>
2018-08-11 18:11:53 +0900

    코드 리팩토링

--- gatsby-node.js
+++ gatsby-node.js
@@ -63,19 +63,38 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
 
   if (node.internal.type === `MarkdownRemark`) {
     const slug = createFilePath({ node, getNode, basePath: `pages` })
-    const dayRegExp = /^\/(19|20)\d{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[0-1])-/
-    const rewriteSlug = '/' + slug.replace(dayRegExp, '')
 
-    let rewriteNode = node
+    const rewriteSlug = slug => {
+      // 폴더 경로에 따라 url에 표시되는 것을 폴더 경로를 제거하고 파일명으로만 url을 지정되도록 하기 위함
+      if (slug.match(/\//g).length > 2) {
+        let tempStr = slug.split('/')
+        slug = `/${tempStr[tempStr.length - 2]}/`
+      }
 
-    if (rewriteNode.frontmatter.tags === undefined) {
-      rewriteNode.frontmatter.tags = []
+      // jekyll 기준으로 파일명에 날짜를 포함시키던 것을 url에서 제거하기 위함
+      const dayRegExp = /\/(19|20)\d{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[0-1])-/
+      if (slug.match(dayRegExp)) {
+        slug = '/' + slug.replace(dayRegExp, '')
+      }
+
+      return slug
     }
+
+    const rewriteNode = node => {
+      // 마크다운 파일 내 태그 필드가 비어있을 시 오류가 나지 않도록 하기 위함
+      if (node.frontmatter.tags === undefined) {
+        node.frontmatter.tags = []
+      }
+      return node;
+    }
+
+    const newSlug = rewriteSlug(slug)
+    const newNode = rewriteNode(node)
 
     createNodeField({
       name: `slug`,
-      node: rewriteNode,
-      value: rewriteSlug,
+      node: newNode,
+      value: newSlug,
     })
   }
 }
