2aa30d7fe57d8ecb72689ebc812d7173ed9b625e
junhobaik <junhobaik@gmail.com>
2018-08-11 17:23:06 +0900

    마크다운 파일 내 tags 필드 없을 시 오류 발생 문제 해결

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,7 +12,10 @@ exports.createPages = ({ graphql, actions }) => {
       graphql(
         `
           {
-            allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }, limit: 1000) {
+            allMarkdownRemark(
+              sort: { fields: [frontmatter___date], order: DESC }
+              limit: 1000
+            ) {
               edges {
                 node {
                   fields {
@@ -20,7 +23,6 @@ exports.createPages = ({ graphql, actions }) => {
                   }
                   frontmatter {
                     title
-                    tags
                   }
                 }
               }
@@ -34,11 +36,12 @@ exports.createPages = ({ graphql, actions }) => {
         }
 
         // Create blog posts pages.
-        const posts = result.data.allMarkdownRemark.edges;
+        const posts = result.data.allMarkdownRemark.edges
 
         _.each(posts, (post, index) => {
-          const previous = index === posts.length - 1 ? null : posts[index + 1].node;
-          const next = index === 0 ? null : posts[index - 1].node;
+          const previous =
+            index === posts.length - 1 ? null : posts[index + 1].node
+          const next = index === 0 ? null : posts[index - 1].node
 
           createPage({
             path: post.node.fields.slug,
@@ -57,18 +60,22 @@ exports.createPages = ({ graphql, actions }) => {
 
 exports.onCreateNode = ({ node, actions, getNode, boundActionCreators }) => {
   // const { createNodeField } = actions
-  const { createNodeField } = boundActionCreators;
+  const { createNodeField } = boundActionCreators
 
   if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages`})
-    const dayRegExp = /^\/(19|20)\d{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[0-1])-/;
-    const rewriteSlug = "/" + slug.replace(dayRegExp, "");
-    
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    const dayRegExp = /^\/(19|20)\d{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[0-1])-/
+    const rewriteSlug = '/' + slug.replace(dayRegExp, '')
+
+    let rewriteNode = node;
+    if(rewriteNode.frontmatter.tags === undefined){
+      rewriteNode.frontmatter.tags = [];
+    }
 
     const value = createFilePath({ node, getNode })
     createNodeField({
       name: `slug`,
-      node,
+      node: rewriteNode,
       value: rewriteSlug,
     })
   }
