e727cec87875b720a606ae10604db26ea09eae1d
junhobaik <junhobaik@gmail.com>
2018-08-11 17:19:52 +0900

    불필요한 import 코드 제거

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 import PostLink from '../components/post-link'
 import Layout from '../components/layout'
 import './index.scss'
