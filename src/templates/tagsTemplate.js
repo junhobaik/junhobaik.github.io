c0e5df6e09b0771ee73a4c35ff80adba6252a408
junhobaik <junhobaik@gmail.com>
2018-08-13 18:43:54 +0900

    불필요한 import 코드 제거

--- src/templates/tagsTemplate.js
+++ src/templates/tagsTemplate.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import PostList from '../components/PostList'
