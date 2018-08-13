f6febdc973734e6d5613584aed4415c4a79b159e
junhobaik <junhobaik@gmail.com>
2018-08-13 18:44:34 +0900

    태그 리스트의 태그 클릭시 하단에 해당 포스트들 출력

--- src/templates/tagListTemplate.js
+++ src/templates/tagListTemplate.js
@@ -1,27 +1,53 @@
 import React, { Component } from 'react'
-import { graphql, Link } from 'gatsby'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
+import PostList from '../components/PostList'
 import './tagListTemplate.scss'
 
 class tagListTemplate extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedTag: null,
+    }
+  }
   render() {
     console.log(this.props)
     const tags = this.props.data.allMarkdownRemark.group
+    const selectedTag = this.state.selectedTag
+
+    const changeSelectedTag = tagName => {
+      this.setState({
+        selectedTag: tagName,
+      })
+    }
 
     const tagList = tags.map(v => {
       return (
-        <Link to={`/tags/${v.fieldValue}`}>
-          <li className="tag">
-            <span className="tag-name">{v.fieldValue}</span>
-            <span className="tag-count">({v.totalCount})</span>
-          </li>
-        </Link>
+        <li
+          key={`tag-${v.fieldValue}`}
+          className="tag"
+          onClick={tagName => changeSelectedTag(v.fieldValue)}
+        >
+          <span className="tag-name">{v.fieldValue}</span>
+          <span className="tag-count">({v.totalCount})</span>
+        </li>
       )
     })
+
+    const postList = (tags, targetTagName) => {
+      const tagsArray = Array.from(tags)
+      for (let v of tagsArray) {
+        if (v.fieldValue === targetTagName) {
+          return <PostList data={v.edges} title={`${v.fieldValue}에 관한 ${v.totalCount}개의 포스트`} />
+        }
+      }
+    }
 
     return (
       <Layout>
         <div className="tag-list">{tagList}</div>
+        {selectedTag ? postList(tags, selectedTag) : null}
       </Layout>
     )
   }
@@ -35,6 +61,19 @@ export const pageQuery = graphql`
       group(field: frontmatter___tags) {
         fieldValue
         totalCount
+        edges {
+          node {
+            excerpt
+            fields {
+              slug
+            }
+            frontmatter {
+              date(formatString: "DD MMMM, YYYY")
+              title
+              tags
+            }
+          }
+        }
       }
     }
   }
