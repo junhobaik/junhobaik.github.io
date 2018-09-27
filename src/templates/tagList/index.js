13efd4349309b759310a7bfadfe608cae52362df
junhobaik <junhobaik@gmail.com>
2018-09-27 18:26:21 +0900

    graphql date format 변경

--- src/templates/tagList/index.js
+++ src/templates/tagList/index.js
@@ -89,7 +89,7 @@ export const pageQuery = graphql`
               slug
             }
             frontmatter {
-              date(formatString: "DD MMMM, YYYY")
+              date(formatString: "YYYY년 MM월 DD일")
               title
               tags
             }
