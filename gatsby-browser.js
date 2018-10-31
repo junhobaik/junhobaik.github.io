6d2474b79b6bb005e6c420dae2f99e6b9fb40918
Junho Baik <junhobaik@gmail.com>
2018-11-01 00:38:03 +0900

    blogPost의 코드블럭 테마 수정

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,4 @@
-require('prismjs/themes/prism-coy.css');
+require('prismjs/themes/prism-tomorrow.css');
+// themes prism- coy, dark, funky, okadia, solarizedlight, tomorrow, twilight
 require('normalize.css/normalize.css');
 require('@fortawesome/fontawesome-svg-core/styles.css');
