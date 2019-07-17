5e1b805cf0468c5f49c34b62fcba017f868f1982
Junho Baik <junhobaik@gmail.com>
2019-07-17 23:37:03 +0900

    global, add google adsense code

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -84,6 +84,19 @@ const Layout = ({ children, data, location }) => (
                 gtag('config', '${config.googleAnalyticsTrackingId}');
               `}
             </script>
+            
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <script>
+              {`
+              (adsbygoogle = window.adsbygoogle || []).push({
+                google_ad_client: "ca-pub-5001380215831339",
+                enable_page_level_ads: true
+              });
+              `}
+            </script>
+
+
+
           </Helmet>
 
           <div id="wrap">
