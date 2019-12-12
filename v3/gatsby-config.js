c683df10a2d4ec57001cc65ace00449e6e76ebe8
Junho Baik <junhobaik@gmail.com>
2019-12-12 21:00:48 +0900

    install&set typography

--- v3/gatsby-config.js
+++ v3/gatsby-config.js
@@ -29,6 +29,12 @@ module.exports = {
       },
     },
     `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
