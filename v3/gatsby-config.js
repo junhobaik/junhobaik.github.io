e175e9736fefe4e118f621808377f0b07938938e
Junho Baik <junhobaik@gmail.com>
2019-12-12 20:50:09 +0900

    edit siteMetadata, gatsby-config

--- v3/gatsby-config.js
+++ v3/gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `JB Blog`,
+    titleTemplate: "%s · JB",
+    description: `Junho Baik's Development Blog`,
+    author: `Junho Baik`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
