d33ea195f3bb430c81f36a4b5c9bdd08c2ab5141
Junho Baik <junhobaik@gmail.com>
2019-12-17 16:30:05 +0900

    Layout, Header 수정
    - 폴더 생성 및 파일 이동, 구조 변경
    - Header 스타일 추가 및 마크업 수정

--- v3/src/components/Header/index.tsx
+++ v3/src/components/Header/index.tsx
@@ -2,6 +2,8 @@ import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import './header.scss';
+
 export interface headerPropsType {
   siteTitle: String;
 }
@@ -10,9 +12,9 @@ const Header = (props: headerPropsType) => {
   const { siteTitle } = props;
 
   return (
-    <header id="Header">
+    <header id="Header" className="header">
       <div>
-        <h1 style={{ margin: 0 }}>
+        <h1 className="header__title-text">
           <Link to="/">{siteTitle}</Link>
         </h1>
       </div>
