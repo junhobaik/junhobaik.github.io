c235d56e6c3bca4e7fa8fe7f68abc264bcc49739
Junho Baik <junhobaik@gmail.com>
2019-12-21 05:31:44 +0900

    manifest 수정

--- v3/gatsby-config.js
+++ v3/gatsby-config.js
@@ -66,13 +66,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: config.title,
+        short_name: config.title,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        background_color: `#fff`,
+        theme_color: `#fff`,
+        display: `standalone`,
       },
     },
     `gatsby-plugin-sass`,
