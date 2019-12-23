83e61db746c86ac7c67a666588ebf1bfa6078086
Junho Baik <junhobaik@gmail.com>
2019-12-23 22:02:15 +0900

    feed(rss) 적용 및 devMode에서 draft 폴더 사용가능하도록 구현

--- v3/gatsby-config.js
+++ v3/gatsby-config.js
@@ -1,7 +1,7 @@
 const config = require('./src/config');
 const { title, description, author, googleAnalytics } = config;
 
-module.exports = {
+const gatsbyConfig = {
   siteMetadata: { title, description, author },
 
   plugins: [
@@ -101,5 +101,71 @@ module.exports = {
     },
 
     `gatsby-plugin-sitemap`,
+
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ 'content:encoded': edge.node.html }],
+                });
+              });
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] }, limit: 10
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: '/rss.xml',
+            title: `${title} | Feed`,
+          },
+        ],
+      },
+    },
   ],
 };
+
+if (process.env.NODE_ENV === 'development') {
+  configs.plugins.push({
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      path: `${__dirname}/_drafts`,
+      name: 'markdown-pages',
+    },
+  });
+}
+
+module.exports = gatsbyConfig;
