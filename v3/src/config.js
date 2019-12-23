b1f8440c68723c015d48444f408cafc5bee8919c
Junho Baik <junhobaik@gmail.com>
2019-12-23 20:41:50 +0900

    config 주석 수정

--- v3/src/config.js
+++ v3/src/config.js
@@ -1,26 +1,28 @@
 module.exports = {
-  // Site MetaData
-  title: `Dev.White`,
-  description: `Junho Baik's Development Blog`,
-  author: `Junho Baik`,
-  siteUrl: 'https://junhobaik.github.io',
+  /** Site MetaData (Required all)*/
+  title: `Dev.White`,                           // (* Required)
+  description: `Junho Baik's Development Blog`, // (* Required)
+  author: `Junho Baik`,                         // (* Required)
+  siteUrl: 'https://junhobaik.github.io',       // (* Required) ex.'https://junhobaik.github.io'
 
-  // Header Prifle image file name "./images/*"
-  profileImageFileName: 'profile.png',
+  /** Header */
+  profileImageFileName: 'profile.png', // include filename extension ex.'profile.jpg'
+    // The Profile image file is located at path "./images/"
+    // If the file does not exist, it is replaced by a random image.
 
-  // Home > Bio
+  /** Home > Bio information*/
   comment: 'Jr. Web Front-end Developer. / javascript, react ...',
   name: 'Junho Baik',
   company: '',
   location: 'Korea',
   email: 'junhobaik@gmail.com',
-  website: 'https://junhobaik.github.io',
-  linkedin: '',
-  facebook: '',
-  instagram: 'https://www.instagram.com/junhobaik',
-  github: 'https://github.com/junhobaik',
+  website: 'https://junhobaik.github.io',           // ex.'https://junhobaik.github.io'
+  linkedin: '',                                     // ex.'https://www.linkedin.com/in/junho-baik-16073a19ab'
+  facebook: '',                                     // ex.'https://www.facebook.com/zuck' or 'https://www.facebook.com/profile.php?id=000000000000000'
+  instagram: 'https://www.instagram.com/junhobaik', // ex.'https://www.instagram.com/junhobaik'
+  github: 'https://github.com/junhobaik',           //ex.'https://github.com/junhobaik'
 
-  // Post
-  enablePostOfContents: true, // TableOfContents
-  disqusShortname: 'junhobaik', // comments (Disqus sort-name)
+  /** Post */
+  enablePostOfContents: true,     // TableOfContents enable (Type of Value: Boolean. Not String)
+  disqusShortname: 'junhobaik',   // comments (Disqus sort-name)
 };
