e5515497a598922f935024a1458c39bd4a2284dd
Junho Baik <junhobaik@gmail.com>
2020-01-19 22:45:08 +0900

    Google Search Console 소유권 확인 추가

--- config.js
+++ config.js
@@ -30,8 +30,9 @@ module.exports = {
   enableSocialShare: true,        // Social share icon activation (Type of Value: Boolean. Not String)
 
   /** Optional */
-  googleAnalytics: '',                  // Google Analytics TrackingID. ex.'UA-123456789-0'
-  googleAdsenseSlot: '',                // Google Adsense Slot. ex.'5214956675'
+  googleAnalytics: '',     // Google Analytics TrackingID. ex.'UA-123456789-0'
+  googleSearchConsole: '', // content value in HTML tag of google search console ownership verification. ex.'w-K42k14_I4ApiQKuVPbCRVV-GxlrqWxYoqO94KMbKo'
+  googleAdsenseSlot: '',   // Google Adsense Slot. ex.'5214956675'
   googleAdsenseClient: '', // Google Adsense Client. ex.'ca-pub-5001380215831339'
     // Please correct the adsense client number(ex.5001380215831339) in the './static/ads.txt' file.
 };
