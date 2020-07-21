9dfa306da4f797ac023b8a0d6eaaf1a3c411aa4a
Junho Baik <junhobaik@gmail.com>
2020-07-21 22:14:56 +0900

    redux 구조 변경

---
+++ src/state/reducer.ts
@@ -0,0 +1,31 @@
+import { ActionTypes, SET_IS_MOBILE, SET_PATH } from './actions';
+
+export interface RootState {
+  path: string;
+  size: string;
+  isMobile: boolean;
+}
+
+export const initialState: RootState = {
+  path: '',
+  size: '25px',
+  isMobile: false,
+};
+
+const reducer = (state: RootState = initialState, action: ActionTypes) => {
+  if (action.type === SET_PATH) {
+    return Object.assign({}, state, {
+      path: action.path || state.path,
+      size: action.size || state.size,
+    });
+  }
+  if (action.type === SET_IS_MOBILE) {
+    return Object.assign({}, state, {
+      isMobile: action.isMobile,
+    });
+  }
+
+  return state;
+};
+
+export default reducer;
