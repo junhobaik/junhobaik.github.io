24b6d8ab1b55de66989168ab7269186162ad9e5e
Junho Baik <junhobaik@gmail.com>
2020-07-29 20:54:58 +0900

    404page, style inline으로 변경
    PageSpeed Insights의 '사용하지 않는 CSS 제거'에 경고가 뜸,
    404 페이지는 사이트맵에 없으므로 나타나는 경고로 보임,
    따라서 스타일 파일이 사용되지 않는 것으로 보는 것으로 추측,
    따라서 해당 스타일 파일을 인라인 스타일으로 변경.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { FontAwesomeIcon as Fa } from '@fortawesome/react-fontawesome';
 import { faFrown } from '@fortawesome/free-regular-svg-icons';
-import './styles/404page.scss';
+
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
 
@@ -9,11 +9,21 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
 
-    <div id="notFound">
-      <div className="icon-wrap">
+    <div
+      id="notFound"
+      style={{
+        maxWidth: '720px',
+        padding: '1rem',
+        margin: '0 auto',
+        marginTop: '3rem',
+        textAlign: 'center',
+        userSelect: 'none',
+      }}
+    >
+      <div className="icon-wrap" style={{ fontSize: '10rem' }}>
         <Fa icon={faFrown} />
       </div>
-      <h1>404</h1>
+      <h1 style={{ fontSize: '5rem', margin: '0.5rem 0 1rem 0' }}>404</h1>
       <p>{`That page doesn't exist or is unavailable.`}</p>
     </div>
   </Layout>
